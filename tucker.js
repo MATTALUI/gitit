edaed0e826443251d36c47188abe53cc6d27406a
Tucker Triggs <tucker.triggs@yahoo.com>
2017-06-06 10:28:14 -0600

    updated tucker and arlo files

---
+++ tucker.js
@@ -0,0 +1,12 @@
+Hello Earthlings!!!
+
+This is a space invader from Neptune, come to take over the world.
+
+Lay down your armaments.
+Forfeit your property.
+Accept your new overlords.
+
+Enjoy your new reality.
+
+Truly yours, 
+Covfefe.
